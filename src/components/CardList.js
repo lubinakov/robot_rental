2d085133ec412357d7a393ddd525361e283a7cd1
lubinakov <lubica.kovacik@gmail.com>
2022-03-11 15:19:31 -0400

    changed CarList component for better UX on mobile devices

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "../components/Card.js";
+import { isMobile } from "react-device-detect";
 
 const CardList = ({ robots }) => {
   return (
-    <div className="grid grid-cols-4 gap-0.9">
+    <div className={isMobile ? "" : "grid grid-cols-4 gap-0.9"}>
       {robots.map((el) => {
         return <Card key={el.id} name={el.name} email={el.email} id={el.id} />;
       })}
